904909975e7c846aa3868d1b34183235880e7eb7
Salim.Said <salembagh131@gmail.com>
2023-08-30 12:54:23 +0300

    add changes in index.html

---
+++ index.js
@@ -0,0 +1,9 @@
+let control = document.querySelectorAll("input")
+
+function changeColor() {
+    if (control.value !== '') {
+                let clear = document.getElementById('sign')
+
+        clear.style.background = "#137fbb";;
+    }
+}
